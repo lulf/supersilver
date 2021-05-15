8ea0051719196f42dae9f357b6ae8fadc416ede9
Ulf Lilleengen <ulf.lilleengen@gmail.com>
2021-05-15 17:56:29 +0200

    Add systemd service

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,7 @@ use core::cmp::{max, min};
 use drogue_device::*;
 use linux_embedded_hal::Pin as PiPin;
 use rotary_encoder_hal::{Direction, Rotary};
+use std::process::Command;
 use sysfs_gpio::Direction as GpioDirection;
 
 #[derive(Device)]
@@ -24,6 +25,7 @@ async fn main(context: DeviceContext<MyDevice>) {
 
     let rot_a = PiPin::new(5);
     let rot_b = PiPin::new(6);
+    let button = PiPin::new(24);
     let led = [PiPin::new(16), PiPin::new(12), PiPin::new(25)];
     for l in led.iter() {
         l.export().expect("Error exporting led pin");
@@ -31,6 +33,13 @@ async fn main(context: DeviceContext<MyDevice>) {
             .expect("Error setting led direction");
     }
 
+    button.export().expect("Error exporting pin button");
+    button
+        .set_direction(GpioDirection::In)
+        .expect("Error setting pin direction button");
+    button
+        .set_active_low(true)
+        .expect("Error setting active low for button");
     rot_a.export().expect("Error exporting pin rot_a");
     rot_a
         .set_direction(GpioDirection::In)
@@ -45,36 +54,59 @@ async fn main(context: DeviceContext<MyDevice>) {
 
     context.mount(|_| {});
 
-    let mut position: i8 = 0;
+    let mut pressed = false;
+    let mut muted = false;
+    let mut pos_muted = 0;
+    let mut position: i16 = 0;
     let mut rotary = Rotary::new(rot_a, rot_b);
     loop {
-        let old_position = position;
-        match rotary.update().unwrap() {
-            Direction::Clockwise => position = min(100, position + 1),
-            Direction::CounterClockwise => position = max(0, position - 1),
-            Direction::None => {}
-        }
-        if old_position != position {
-            for i in 0..led.len() {
-                if i == (position as usize) % 3 {
-                    led[i].set_value(1).unwrap();
-                } else {
-                    led[i].set_value(0).unwrap();
-                }
+        // Debounce button
+        if button.get_value().unwrap() != 0 && !pressed {
+            log::info!("Button pressed");
+            pressed = true;
+            // Check if we should be muted
+            if !muted {
+                log::info!("Muting");
+                muted = true;
+                set_volume(0);
+                pos_muted = position;
+            } else {
+                log::info!("Unmuting");
+                muted = false;
+                position = pos_muted;
             }
-            log::info!("Position: {}", position);
+        } else if button.get_value().unwrap() == 0 {
+            pressed = false;
         }
-        /*
-        led_blue
-            .set_value(value % 2)
-            .expect("Error setting led value");
 
-        // Send that completes immediately when message is enqueued
-        a_addr.notify(SayHello("World")).unwrap();
-        // Send that waits until message is processed
-        b_addr.request(SayHello("You")).unwrap().await;
-
-        value += 1;
-        */
+        // Check volume position
+        if !muted {
+            let old_position = position;
+            match rotary.update().unwrap() {
+                Direction::Clockwise => position = min(255, position + 10),
+                Direction::CounterClockwise => position = max(0, position - 10),
+                Direction::None => {}
+            }
+            if old_position != position {
+                for i in 0..led.len() {
+                    if i == (position as usize) % 3 {
+                        led[i].set_value(1).unwrap();
+                    } else {
+                        led[i].set_value(0).unwrap();
+                    }
+                }
+                log::info!("Position: {}", position);
+                set_volume(position as u8);
+            }
+        }
     }
+}
+
+fn set_volume(level: u8) {
+    let output = Command::new("sh")
+        .arg("-c")
+        .arg(format!("amixer set DSPVolume {}", level))
+        .output()
+        .expect("failed to execute process");
+    log::info!("{:#?}", output);
 }
